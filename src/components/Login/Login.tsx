a81e87afc34df028e2d1d58b0a80c974afbcd4a8
geronimium_gr <geronimoadalia@gmail.com>
2022-03-07 00:00:49 +0800

    Update Login.tsx
    Read

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,6 +1,17 @@
 import React from "react";
 import './Login.css'
 
+
+// Suggested Naming Convention
+// New Component - ComponentName.tsx
+// New CSS - ComponentName.css
+
+// - Every component must have designated folder to become more organized and to avoid confusion.
+// Example:
+// Folder: Login
+//  -- Login.tsx
+//  -- Login.css
+
 const Login = () => {
 
 }
